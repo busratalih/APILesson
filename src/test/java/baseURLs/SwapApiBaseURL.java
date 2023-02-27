984e0bb550e9ac703bac5394eb3098bd781329b4
Büşra Talih <busra.talihhh@gmail.com>
2023-02-27 21:36:40 +0300

    27.02.2023

---
+++ src/test/java/baseURLs/SwapApiBaseURL.java
@@ -0,0 +1,21 @@
+package baseURLs;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Before;
+
+public class SwapApiBaseURL {
+
+    // BASE URL  ====>>>> https://swapi.dev/api
+
+    protected RequestSpecification specification;
+
+    @Before
+    public void setUpBaseURL(){
+
+        specification = new RequestSpecBuilder().
+                setBaseUri("https://swapi.dev/api").
+                build();
+
+    }
+}
